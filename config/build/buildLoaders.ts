8d0bf86ba1c111a63384457cdd87092d7ebeb158
Mher Hovhannisyan <mh3r99@gmail.com>
2023-06-01 23:32:35 +0400

    add React and css webpack

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,7 +1,19 @@
 import { RuleSetRule } from 'webpack';
 
 export function buildLoaders(): RuleSetRule[] {
+  const cssLoader = {
+    test: /\.s[ac]ss$/i,
+    use: [
+      // Creates `style` nodes from js strings
+      'style-loader',
+      // Translates CSS into CommonJS
+      'css-loader',
+      // Compiles Sass to CSS
+      'sass-loader',
+    ],
+  };
+
   const typeScriptLoader = { test: /\.tsx?$/, use: 'ts-loader', exclude: /node_modules/ };
 
-  return [typeScriptLoader];
+  return [typeScriptLoader, cssLoader];
 }
--- src/test.ts
+++
@@ -1,5 +0,0 @@
-export function someFn(arg: number): string {
-  console.log('test', arg);
-
-  return 'test aaaa';
-}
