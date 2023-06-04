15249b378234b48b47e5a69856ed0c19e5f699f6
Mher Hovhannisyan <mh3r99@gmail.com>
2023-06-04 16:50:24 +0400

    dark and light theme with context

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import { Suspense } from 'react';
-import './index.scss';
 import { Route, Routes } from 'react-router-dom';
-import AboutPage from './pages/AboutPage/AboutPage';
-import MainPage from './pages/MainPage/MainPage';
 import { Link } from 'react-router-dom';
 import { AboutPageAsync } from './pages/AboutPage/AboutPage.async';
 import { MainPageAsync } from './pages/MainPage/MainPage.async';
+import './styles/index.scss';
+import { useTheme } from './theme/useTheme';
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={'/about'}>About page</Link>
       <Link to={'/'}>Home page</Link>
       <Suspense fallback={<div>Loading...</div>}>
