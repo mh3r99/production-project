94e59cb422f43dda8803e8455d786b205464f8f9
Mher Hovhannisyan <mh3r99@gmail.com>
2023-06-10 21:22:12 +0400

    App Sidebar and Layout

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,13 +3,17 @@ import './styles/index.scss';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from './providers/router';
 import { Navbar } from 'widgets/Navbar';
+import { Sidebar } from 'widgets/Sidebar';
 
 export const App = () => {
   const { theme } = useTheme();
   return (
     <div className={classNames('app', {}, [theme])}>
       <Navbar />
-      <AppRouter />
+      <div className="content-page">
+        <Sidebar />
+        <AppRouter />
+      </div>
     </div>
   );
 };
