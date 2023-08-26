45165d740273b6a62070edb55892d057f21c8dad
Mher Hovhannisyan <mh3r99@gmail.com>
2023-08-26 23:50:35 +0400

    Commet entities

---
+++ src/entities/Comment/ui/CommentList/CommentList.tsx
@@ -0,0 +1,30 @@
+import React, { memo } from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import { Comment } from 'entities/Comment';
+import { Text } from 'shared/ui/Text/Text';
+import { useTranslation } from 'react-i18next';
+import cls from './CommentList.module.scss';
+import { CommentCard } from '../CommentCard/CommentCard';
+
+interface CommentListProps{
+  className?:string;
+  comments?: Comment[];
+  isLoading?:boolean
+}
+
+export const CommentList = memo(({ className, comments, isLoading }:CommentListProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.CommentList, {}, [className])}>
+            {comments?.length ? comments.map((comment) => (
+                <CommentCard
+                    key={comment.id}
+                    className={cls.comment}
+                    isLoading={isLoading}
+                    comment={comment}
+                />
+            )) : <Text text={t('Комментарии отсутствуют')} />}
+        </div>
+    );
+});
--- src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -2,6 +2,10 @@ import React, { memo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { ArticleDetails } from 'entities/Article';
 import { useParams } from 'react-router-dom';
+import { Text } from 'shared/ui/Text/Text';
+import { CommentList } from 'entities/Comment';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './ArticleDetailsPage.module.scss';
 
 const ArticleDetailsPage = () => {
     const { t } = useTranslation('article-details');
@@ -12,8 +16,30 @@ const ArticleDetailsPage = () => {
     }
 
     return (
-        <div>
+        <div className={classNames(cls.ArticleDetailsPage, {}, [])}>
             <ArticleDetails id={id} />
+            <Text className={cls.commentTitle} title={t('Комментарии')} />
+            <CommentList
+                isLoading
+                comments={[{
+                    id: '1',
+                    text: 'comment 1',
+                    user: {
+                        id: '1',
+                        username: 'admin123',
+                        avatar: 'https://techcrunch.com/wp-content/uploads/2021/01/GettyImages-1005344670.jpg',
+                    },
+
+                }, {
+                    id: '2',
+                    text: 'comment 2',
+                    user: {
+                        id: '2',
+                        username: 'ankap user',
+                        avatar: 'https://passportforrussians.com/wp-content/uploads/2021/10/web2-min.jpg',
+                    },
+                }]}
+            />
         </div>
     );
 };
