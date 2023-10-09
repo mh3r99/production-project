a7e44b4ed98e12ffe378e615bcb849730d5e3c3d
Mher Hovhannisyan <mh3r99@gmail.com>
2023-10-09 22:07:33 +0400

    fix useInfiniteScroll

--- src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
+++ src/shared/lib/hooks/useInfiniteScroll/useInfiniteScroll.ts
@@ -20,7 +20,7 @@ export function useInfiniteScroll({
         if (callback) {
             const options = {
                 root: wrapperElement,
-                rootMargin: '0px',
+                rootMargin: '1px',
                 threshold: 1.0,
             };
 
