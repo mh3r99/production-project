08c7c49a367691c22fd8c1cbfbea54cdff3062e3
Mher Hovhannisyan <mh3r99@gmail.com>
2023-11-22 23:29:29 +0400

    Dropdown, user avatar

--- src/entities/Country/ui/CountrySelect/CountrySelect.tsx
+++ src/entities/Country/ui/CountrySelect/CountrySelect.tsx
@@ -37,7 +37,7 @@ export const CountrySelect = memo(({
             items={options}
             onChange={onChangeHandler}
             readonly={readonly}
-            direction="top"
+            direction="top right"
 
         />
     );
