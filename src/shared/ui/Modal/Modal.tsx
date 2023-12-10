0a9a395038036ddb9dcb6b5bfcc18e190c3fd15d
Mher Hovhannisyan <mh3r99@gmail.com>
2023-12-10 16:03:14 +0400

    Drawer, Overlay, React-device-detect.

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,6 +3,7 @@ import React, {
 } from 'react';
 import { Mods, classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { Overlay } from '../Overlay/Overlay';
 import cls from './Modal.module.scss';
 import Portal from '../Portal/Portal';
 
@@ -41,10 +42,6 @@ const Modal: React.FC<ModalProps> = ({
         }
     }, [onClose]);
 
-    const onContentClick = (e:React.MouseEvent) => {
-        e.stopPropagation();
-    };
-
     const onKeyDown = useCallback((e:KeyboardEvent) => {
         if (e.key === 'Escape') {
             closeHandler();
@@ -74,9 +71,8 @@ const Modal: React.FC<ModalProps> = ({
     return (
         <Portal>
             <div className={classNames(cls.Modal, mods, [className, theme, 'app_modal'])}>
-                <div className={cls.overlay} onClick={closeHandler}>
-                    <div className={cls.content} onClick={onContentClick}>{children}</div>
-                </div>
+                <Overlay onClick={closeHandler} />
+                <div className={cls.content}>{children}</div>
             </div>
         </Portal>
     );
