f809aa5c2f31b5a9457221bd7a174a66dbbd9f03
Mher Hovhannisyan <mh3r99@gmail.com>
2023-12-17 16:47:05 +0400

    remove virtualized ArticleList

--- src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
+++ src/features/articleRecommendationsList/ui/ArticleRecommendationsList/ArticleRecommendationsList.tsx
@@ -26,7 +26,6 @@ export const ArticleRecommendationsList = memo((props: ArticleRecommendationsLis
             <ArticleList
                 articles={articles}
                 target="_blank"
-                virtualized={false}
             />
         </VStack>
     );
