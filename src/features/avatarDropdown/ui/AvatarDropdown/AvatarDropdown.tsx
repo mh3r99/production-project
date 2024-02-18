0fcfb6ee7bf23f8bc4a5bee4d5e9e59702d81cdb
Mher Hovhannisyan <mh3r99@gmail.com>
2024-02-18 23:20:18 +0400

    redesign card, popups, header

--- src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
+++ src/features/avatarDropdown/ui/AvatarDropdown/AvatarDropdown.tsx
@@ -1,8 +1,8 @@
 import React, { useCallback } from 'react';
 import { useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
-import { Dropdown } from '@/shared/ui/deprecated/Popups';
-import { Avatar } from '@/shared/ui/deprecated/Avatar';
+import { Dropdown as DropdownDeprecated } from '@/shared/ui/deprecated/Popups';
+import { Avatar as AvatarDeprecated } from '@/shared/ui/deprecated/Avatar';
 import {
     getUserAuthData,
     isUserAdmin,
@@ -11,6 +11,9 @@ import {
 } from '@/entities/User';
 import { getRouteAdmin, getRouteProfile } from '@/shared/const/router';
 import { useAppDispatch } from '@/shared/lib/hooks/useAppDispatch/useAppDispatch';
+import { ToggleFeatures } from '@/shared/lib/features/ToggleFeatures/ToggleFeatures';
+import { Dropdown } from '@/shared/ui/redesigned/Popups';
+import { Avatar } from '@/shared/ui/redesigned/Avatar';
 
 export const AvatarDropdown = () => {
     const { t } = useTranslation();
@@ -29,29 +32,47 @@ export const AvatarDropdown = () => {
         return null;
     }
 
+    const items = [
+        ...(isAdminPanelAvailable
+            ? [
+                  {
+                      content: t('Админка'),
+                      href: getRouteAdmin(),
+                  },
+              ]
+            : []),
+        {
+            content: t('Профиль'),
+            href: getRouteProfile(authData.id),
+        },
+        {
+            content: t('Выйти'),
+            onClick: onLogout,
+        },
+    ];
+
     return (
-        <Dropdown
-            direction="bottom left"
-            items={[
-                ...(isAdminPanelAvailable
-                    ? [
-                          {
-                              content: t('Админка'),
-                              href: getRouteAdmin(),
-                          },
-                      ]
-                    : []),
-                {
-                    content: t('Профиль'),
-                    href: getRouteProfile(authData.id),
-                },
-                {
-                    content: t('Выйти'),
-                    onClick: onLogout,
-                },
-            ]}
-            trigger={
-                <Avatar fallbackInverted size={30} src={authData.avatar} />
+        <ToggleFeatures
+            feature="isAppRedesigned"
+            on={
+                <Dropdown
+                    direction="bottom left"
+                    items={items}
+                    trigger={<Avatar size={40} src={authData.avatar} />}
+                />
+            }
+            off={
+                <DropdownDeprecated
+                    direction="bottom left"
+                    items={items}
+                    trigger={
+                        <AvatarDeprecated
+                            fallbackInverted
+                            size={30}
+                            src={authData.avatar}
+                        />
+                    }
+                />
             }
         />
     );
--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -1,13 +1,20 @@
 import React, { memo, useCallback, useState } from 'react';
 import { BrowserView, MobileView } from 'react-device-detect';
-import { Button, ButtonTheme } from '@/shared/ui/deprecated/Button';
-import { Icon } from '@/shared/ui/deprecated/Icon';
-import { Popover } from '@/shared/ui/deprecated/Popups';
+import {
+    Button as ButtonDeprecated,
+    ButtonTheme,
+} from '@/shared/ui/deprecated/Button';
+import { Icon as IconDeprecated } from '@/shared/ui/deprecated/Icon';
+import { Popover as PopoverDeprecated } from '@/shared/ui/deprecated/Popups';
 import { Drawer } from '@/shared/ui/deprecated/Drawer';
-import NotificationIcon from '@/shared/assets/icons/notification-20-20.svg';
+import NotificationIconDeprecated from '@/shared/assets/icons/notification-20-20.svg';
+import NotificationIcon from '@/shared/assets/icons/notification.svg';
 import { NotificationList } from '@/entities/Notification';
 import { classNames } from '@/shared/lib/classNames/classNames';
 import cls from './NotificationButton.module.scss';
+import { ToggleFeatures } from '@/shared/lib/features/ToggleFeatures/ToggleFeatures';
+import { Icon } from '@/shared/ui/redesigned/Icon';
+import { Popover } from '@/shared/ui/redesigned/Popups';
 
 interface NotificationButtonProps {
     className?: string;
@@ -26,21 +33,57 @@ export const NotificationButton = memo(
         }, []);
 
         const trigger = (
-            <Button onClick={onOpenDrawer} theme={ButtonTheme.CLEAR}>
-                <Icon Svg={NotificationIcon} inverted />
-            </Button>
+            <ToggleFeatures
+                feature="isAppRedesigned"
+                on={
+                    <Icon
+                        Svg={NotificationIcon}
+                        clickable
+                        onClick={onOpenDrawer}
+                    />
+                }
+                off={
+                    <ButtonDeprecated
+                        onClick={onOpenDrawer}
+                        theme={ButtonTheme.CLEAR}
+                    >
+                        <IconDeprecated
+                            Svg={NotificationIconDeprecated}
+                            inverted
+                        />
+                    </ButtonDeprecated>
+                }
+            />
         );
 
         return (
             <>
                 <BrowserView>
-                    <Popover
-                        className={classNames('', {}, [className])}
-                        direction="bottom left"
-                        trigger={trigger}
-                    >
-                        <NotificationList className={cls.notifications} />
-                    </Popover>
+                    <ToggleFeatures
+                        feature="isAppRedesigned"
+                        on={
+                            <Popover
+                                className={classNames('', {}, [className])}
+                                direction="bottom left"
+                                trigger={trigger}
+                            >
+                                <NotificationList
+                                    className={cls.notifications}
+                                />
+                            </Popover>
+                        }
+                        off={
+                            <PopoverDeprecated
+                                className={classNames('', {}, [className])}
+                                direction="bottom left"
+                                trigger={trigger}
+                            >
+                                <NotificationList
+                                    className={cls.notifications}
+                                />
+                            </PopoverDeprecated>
+                        }
+                    />
                 </BrowserView>
                 <MobileView>
                     {trigger}
