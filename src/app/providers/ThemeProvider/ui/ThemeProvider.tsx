0768030ae75074922ee65edbc884c46754d36411
Mher Hovhannisyan <mh3r99@gmail.com>
2024-03-13 23:55:27 +0400

    fix ArticleListItemSkeleton, scrollbar redesigned

--- src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
+++ src/app/providers/ThemeProvider/ui/ThemeProvider.tsx
@@ -25,6 +25,10 @@ const ThemeProvider: React.FC<ThemeProviderProps> = ({
         }
     }, [defaultTheme, isThemeInited]);
 
+    useEffect(() => {
+        document.body.className = theme;
+    }, [theme]);
+
     const defaultProps = useMemo(() => ({ theme, setTheme }), [theme]);
 
     return (
